a0f760915496886792c10e890ba0059cd34a0962
rasuku0402 <taitoo0402@gmail.com>
2023-08-22 18:04:55 +0900

    Initial Commit

---
+++ Noname/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noname
+//
+//  Created by 菊地泰斗 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
