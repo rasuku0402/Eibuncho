8626c784fc734cf22eccfcbb94dde2ee9cfc6e69
oremayaneu <ore.mayaneu@gmail.com>
2023-09-14 19:05:00 +0900

    #8

---
+++ Noname/CreateAccountViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateAccountViewController.swift
+//  Noname
+//
+//  Created by 釆山怜央 on 2023/09/11.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtnAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
